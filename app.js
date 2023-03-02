9e6083a671c7504b242a7d254ed755e992e0df82
erinknight242 <erinknight242@gmail.com>
2023-03-02 17:01:34 -0600

    Turn off socket mode

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { App } = require('@slack/bolt');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
+  socketMode: false,
   appToken: process.env.SLACK_APP_TOKEN
 });
 
